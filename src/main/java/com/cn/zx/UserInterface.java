21ae2bd915f16c9dd69b5351eb9f84341e172979
zhang.xu <zhang.xu@all-union.com.cn>
2016-12-07 18:13:56 +0800

    first commit

---
+++ src/main/java/com/cn/zx/UserInterface.java
@@ -0,0 +1,10 @@
+package com.cn.zx;
+
+/**
+ * Created by Administrator on 2016/12/7.
+ * Dubbo对外暴露的接口
+ */
+public interface UserInterface {
+    void sayHello(String username);
+    int getAge();
+}
